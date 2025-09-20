d211447bde0b55c92bee302114730d76c7c7170d
Quyen Luc <quyenlluc@0606@gmail.com>
2025-09-20 19:26:58 +0700

    update code: sửa favicon và layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,34 @@
 
+// import "./globals.css";
+// import { AuthProvider } from "@/contexts/auth";
+
+// // ✅ metadata cho favicon & title
+// export const metadata = {
+//   title: "Cổng thông tin đào tạo",
+//   description: "Trang web Next.js",
+//   // icons: {
+//   //   icon: "/neuu.png", // favicon nằm trong thư mục public/
+//   // },
+// };
+
+// export default function RootLayout({ children }: { children: React.ReactNode }) {
+//   return (
+//     <AuthProvider>
+//       <html lang="vi">
+//         <body className="flex h-screen bg-gray-100">
+//           {children}
+//         </body>
+//       </html>
+//     </AuthProvider>
+//   );
+// }
+// app/layout.tsx
 import "./globals.css";
 import { AuthProvider } from "@/contexts/auth";
 
-// ✅ metadata cho favicon & title
 export const metadata = {
   title: "Cổng thông tin đào tạo",
   description: "Trang web Next.js",
-  icons: {
-    icon: "/neu.jpg", // favicon nằm trong thư mục public/
-  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
